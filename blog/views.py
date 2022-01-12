419a602db539b79863def7c4343182142f5546d8
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 10:34:17 +0300

    fetched blog post from db to show in template

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'blog/index.html')
+
+    posts = Article.objects.filter(status=1).order_by('-created_on')
+
+    context = {
+        'posts' : posts
+    }
+
+    return render(request, 'blog/index.html', context)
