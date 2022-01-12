0324d23d58505dc0580d4a7000f7e0ff13f00298
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 11:52:07 +0300

    created view for blog articles to be display selected article from db on article.html

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.views import generic
 from .models import *
 
 # Create your views here.
 
+# home/index.html view
 def home(request):
 
     posts = Article.objects.filter(status=1).order_by('-created_on')
@@ -12,3 +14,8 @@ def home(request):
     }
 
     return render(request, 'blog/index.html', context)
+
+# blog post/article.html view
+class ArticleDetail(generic.DetailView):
+    model = Article
+    template_name = 'blog/article.html'
