75db7ae622833bc27862e27b89a17b5ed176b398
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 10:08:29 +0300

    registered db to admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status','tags', 'created_on')
+    list_filter = ('status', 'tags')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(Article, ArticleAdmin)
