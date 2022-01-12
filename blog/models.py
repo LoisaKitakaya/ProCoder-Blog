cdb4b83807e2b0db48dccd85ae13daf295734aba
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 15:22:24 +0300

    created new datafield for images and made migrations to the db

--- blog/models.py
+++ blog/models.py
@@ -23,6 +23,7 @@ class Article(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
+    thumbnail = models.ImageField(upload_to='thumbnails/')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
