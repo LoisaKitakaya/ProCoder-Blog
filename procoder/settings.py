a663c4c15123e8fde51596e284250699a1b1c552
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 23:00:22 +0300

    heroku changes

--- procoder/settings.py
+++ procoder/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hw+rdi0@7dbq#f*37#c@3bcdjko-s^htu8tb%b*&92slibv#)='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'procoderblog.herokuapp.com']
 
