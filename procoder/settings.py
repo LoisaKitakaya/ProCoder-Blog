45084aa30c6b8b57a0cb92ef0a45bf7171b30284
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 22:22:42 +0300

    heroku changes

--- procoder/settings.py
+++ procoder/settings.py
@@ -124,9 +124,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # media files (Images, Documents)
 MEDIA_URL = '/media/'
