c33db4a1399432adc3a6208d36744e8ec1c8c410
Freedom Loisa <kitakayaloisa@gmail.com>
2022-01-12 19:21:52 +0300

    major style changes

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models import *
+from .forms import *
 
 # Create your views here.
 
